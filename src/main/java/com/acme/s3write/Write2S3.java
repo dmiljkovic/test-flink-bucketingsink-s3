0089dcd1175700a010c3c2e00c75d396f8bb2a7d
Dejan Miljkovic <dmiljkovic@MBP15-9AHTD6.local>
2018-03-26 10:59:06 +0300

    Changed pom for Flink 1.4.2. Some code cleanup

--- src/main/java/com/acme/s3write/Write2S3.java
+++ src/main/java/com/acme/s3write/Write2S3.java
@@ -1,13 +1,11 @@
 package com.acme.s3write;
 
 import org.apache.flink.api.java.utils.ParameterTool;
-import org.apache.flink.runtime.state.filesystem.FsStateBackend;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 
 import java.util.*;
 
-//com.acme.s3write.Write2S3
 public class Write2S3 {
     private static DataStream<String> ds1(StreamExecutionEnvironment env) {
         List<String> rec = new ArrayList<>();
@@ -23,17 +21,12 @@ public class Write2S3 {
     public static void main(String[] args) throws Exception {
         S3SinkConfig s3conf = new S3SinkConfig();
         s3conf.setHadoopconf("resurces");
-        s3conf.setS3Uri("s3://xxxxx");
+        s3conf.setS3Uri("s3://xxxxxx/");
         s3conf.setSplitDateFormat("yyyy-MM-dd");
         s3conf.setFilePrefix("file-");
         s3conf.setFileSufix(".json");
 
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
-        //https://ci.apache.org/projects/flink/flink-docs-release-1.3/ops/state_backends.html#available-state-backends
-        //env.setStateBackend(new MemoryStateBackend(1073741824, true));
-        //needed for S3 write
-        env.setStateBackend(new FsStateBackend("file:///tmp", true));
-
 
         Map par = new HashMap();
         par.put("fs.hdfs.hadoopconf", s3conf.getHadoopconf());
